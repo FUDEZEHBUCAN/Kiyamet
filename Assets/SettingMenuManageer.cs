37d64e7a0617731483dd9aa05109eb1ba532db45
EderenK <derenelfk@gmail.com>
2026-04-08 21:24:08 +0300

    Settings UI

---
+++ Assets/SettingMenuManageer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class SettingMenuManageer : MonoBehaviour
+{
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+}
